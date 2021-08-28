702bf2db2813457dc70606f682e1cf4278dd4141
jugal <jugalsutharpro@gmail.com>
2021-08-28 13:46:14 +0530

    selection sort card added

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,10 +5,11 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Button from 'react-bootstrap/Button'
 import bubble from "../images/bubble.jfif"
 import {Link} from "@reach/router"
+import Selection from "../images/selection.png"
 function Home() {
   return (
     <div className="home">
-      <Card style={{ width: "18rem" }}>
+      <Card className="sort_card" style={{ width: "18rem" }}>
         <Card.Img variant="top" src={bubble}/>
         <Card.Body>
           <Card.Title>Bubble Sort</Card.Title>
@@ -18,6 +19,18 @@ function Home() {
           <Button variant="primary"><Link className="link" to="/bubblesort">GO SOMEWHERE</Link></Button>
         </Card.Body>
       </Card>
+
+      <Card className="sort_card" style={{ width: "18rem" }}>
+        <Card.Img variant="top" src={Selection}/>
+        <Card.Body>
+          <Card.Title>Selection Sort</Card.Title>
+          <Card.Text>
+             Selection Sort Algorithm Visualization
+          </Card.Text>
+          <Button variant="primary"><Link className="link" to="#">GO SOMEWHERE</Link></Button>
+        </Card.Body>
+      </Card>
+
     </div>
   );
 }
