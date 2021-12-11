ee77fb905b5380869c277aa7737f4ae8839bf83f
jugal <jugalsutharpro@gmail.com>
2021-12-11 12:42:56 +0530

    bug fixed

--- src/App.js
+++ src/App.js
@@ -8,13 +8,7 @@ import SearchingSorting from "./components/SearchingSorting"
 import DataStructure from "./components/DataStructure"
 import Nqueen from "./components/Nqueen"
 import Stack from "./components/Stack"
-<<<<<<< HEAD
-import Queue from "./components/Queue"
-import LinearSearch from './Algorithm/Searching/LinearSearch'
-
-=======
 import Tree from "./components/Tree/Tree"
->>>>>>> 47fdf8a (binary search tree and bugs fixed in path finding)
 function App() {
   return (
     <div>
@@ -27,12 +21,7 @@ function App() {
            <Pathfinding path="/pathfinding" />
            <Nqueen path="/nqueen" />
            <Stack path="/stack" />
-<<<<<<< HEAD
-           <Queue path="/queue" />
-           <LinearSearch path="/linearsearch" />
-=======
            <Tree path="/tree"/>
->>>>>>> 47fdf8a (binary search tree and bugs fixed in path finding)
          </Router>
     </div>
   )
